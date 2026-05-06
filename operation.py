b3b2a3c227dd590ff72086e75bd271ebc6d636c8
chetan8982 <chetant1902@gmail.com>
2026-05-06 07:35:24 +0530

    upadate file

---
+++ operation.py
@@ -0,0 +1,37 @@
+# name = 'dev'
+# age= 25
+# city = 'jaipur'
+# hobby= 'cooking'
+
+# Meet Dev, a 25-year-old enthusiast from Jaipur. When not busy with daily tasks, Dev loves spending time cooking. 
+# Life in Jaipur keeps Dev energetic and curious every single day. 
+# With coding as a passion, the future looks creative and inspiring for Dev in the Jaipur City. 
+
+# print(f""" Meet {name}, a {age}-year-old enthusiast from {city}. When not busy with daily tasks, {name} loves spending time {hobby}. 
+# Life in {city} keeps {name} energetic and curious every single day. 
+# With coding as a passion, the future looks creative and inspiring for {name} in the {city} City.""")
+
+
+# q=32
+# name=input("Enter Full Name : ")
+# prof=input("Enter Profession : ")
+# fvrt_qt=input("Enter Favourite quote : ")
+# yr_ex=int(input("Enter Year of Experience : "))
+
+# print(f"""Name: {name}
+# Profession: {prof}
+# Experience: {yr_ex} years
+# Quote: {fvrt_qt} """)
+ 
+
+# # q=33
+# mv_name=input("Enter movie name: ")
+# vwr_name=input("Enter viewer name: ")
+# seat_no=int(input("Enter seat number: "))
+# show_ti=input("Enter time (HH:MM): ")
+
+# print(f"""Movie: {mv_name}
+# Viewer: {vwr_name}
+# Seat Number: {seat_no}
+# Time: {show_ti}
+# Enjoy the show! """)
